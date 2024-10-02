4d8744b9712b5421cab494ba6a48b6e7aba68509
Cole Dorman <coledorman12@gmail.com>
2024-10-02 15:32:20 -0500

    fog no longer falls off the world and player now moves

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 1;
+    private Rigidbody2D _rg;
+    private GameObject _playerArt;
+    private Vector3 _playerInput;
+
     void Start()
     {
-        
+        //_rg = this.GetComponentInChildren<Rigidbody2D>();
+        //_playerArt = this.transform.Find("TankArt").gameObject;
     }
-
-    // Update is called once per frame
+    /// <summary>
+    /// Get player input
+    /// </summary>
     void Update()
     {
-        
+        _playerInput = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+    }
+    /// <summary>
+    /// face player toward movement direction and linearly translate players position over time
+    /// </summary>
+    private void FixedUpdate()
+    {
+        transform.position += _playerInput * (speed * Time.deltaTime); 
     }
 }
