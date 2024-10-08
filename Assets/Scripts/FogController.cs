6fcc55c5f86ce700d194dfb076455e724d21aa6a
Cole Dorman <coledorman12@gmail.com>
2024-10-08 14:35:52 -0500

    Working!!!

--- Assets/Scripts/FogController.cs
+++ Assets/Scripts/FogController.cs
@@ -17,6 +17,7 @@ namespace DefaultNamespace
         private float _initialTime;
         private float _currentTime;
         private float _time;
+        private BoxCollider2D _fogCollider;
 
         private void Start()
         {
@@ -25,18 +26,21 @@ namespace DefaultNamespace
             initialScaleRight = fogObjectRight.transform.localScale;
             //_initialTime = Time.time;
             //_endTime = Time.time + duration;
+            _fogCollider = fogObjectLeft.GetComponentInChildren<BoxCollider2D>();
         }
 
         public void Update()
         {
-            _time += Time.deltaTime;
-            float t = Mathf.Clamp01(_time / duration);
-            print(t);
+            if (_fogCollider.bounds.max.x <= 0) //check if the bounds extends to 0 x pos
+            {
+                _time += Time.deltaTime;
+                float t = Mathf.Clamp01(_time / duration);
+                print(t);
+                var xScalerFloat = Mathf.Lerp(initialScaleRight.x, initialScaleRight.x + Mathf.Abs(fogObjectRight.transform.position.x), t); //-85 & 85
             
-            var _xScaler = Mathf.Lerp(initialScaleRight.x, initialScaleRight.x + Mathf.Abs(fogObjectRight.transform.position.x), t); //-85 & 85
-            
-            fogObjectRight.transform.localScale = new Vector3(_xScaler, fogObjectRight.transform.localScale.y, 1);
-            fogObjectLeft.transform.localScale = new Vector3(_xScaler, fogObjectLeft.transform.localScale.y, 1);
+                fogObjectRight.transform.localScale = new Vector3(xScalerFloat, fogObjectRight.transform.localScale.y, 1);
+                fogObjectLeft.transform.localScale = new Vector3(xScalerFloat, fogObjectLeft.transform.localScale.y, 1); 
+            }
         }
     }
 }
