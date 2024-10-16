7913685e6eb809b1753da54deef0bebf59207e92
Cole Dorman <coledorman12@gmail.com>
2024-10-16 15:18:42 -0500

    testing new camera movement

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,10 +6,20 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
     public float yOffset = 1.0f;
+    private Vector3 mousePos;
+    private Camera _mainCam;
+
+    private void Start()
+    {
+        _mainCam = Camera.main;
+    }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.position + new Vector3(0, yOffset, -10);
+        mousePos = Input.mousePosition; //TODO make it stick to the player more
+        //TODO test out camera movement regarding shifting player to the edge while moving to increase forward vision
+        Vector2 targetPosition = _mainCam.ScreenToWorldPoint(Input.mousePosition); //convert to world coordinates
+        transform.position = player.position + new Vector3((0+targetPosition.x/2), yOffset, -10);
     }
 }
