fd2165448634064eeb120c5d705c56d9455e72c7
Cole Dorman <coledorman12@gmail.com>
2024-10-16 11:18:22 -0500

    Refactor variable names for clarity and consistency
    Updated variable names in BasicShooter.cs to follow consistent and clearer naming conventions. This improves code readability and aligns with standard coding practices.

--- Assets/Scripts/BasicShooter.cs
+++ Assets/Scripts/BasicShooter.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 
 public class BasicShooter : MonoBehaviour
 {
-    public GameObject Projectile;
-    public float _verticalOffset = 0.5f;
+    public GameObject projectile;
+    public float verticalOffset = 0.5f;
     public float fireRate = 0.3f;
     //public AudioSource shoot;
     private float _speed = 500;
 
-    private float nextFire = 0;
+    private float _nextFire = 0;
     /// <summary>
     /// Basic Shooter defaults to firing away from the center of the world position
     /// </summary>
     void Update()
     {
-        if (Time.time >= nextFire)
+        if (Time.time >= _nextFire)
         {
-            GameObject bullet = Instantiate(Projectile, transform.position, quaternion.identity);
+            GameObject bullet = Instantiate(projectile, transform.position, quaternion.identity);
             //shoot.Play();
-            var bulletRg = bullet.GetComponent<Rigidbody2D>();
-            var direction = (transform.position.x > 0 ? transform.right : -transform.right);
+            Rigidbody2D bulletRg = bullet.GetComponent<Rigidbody2D>();
+            Vector3 direction = (transform.position.x > 0 ? transform.right : -transform.right);
             bulletRg.AddForce(direction*_speed);
-            nextFire = Time.time + fireRate;
+            _nextFire = Time.time + fireRate;
         }
     }
 }
