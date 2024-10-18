7757612ee45af93422c18d1f24df135b1afeb7f9
John Carlson <johnathonccontact@gmail.com>
2024-10-18 14:43:33 -0500

    Reworked Item UI Display
    Made it so that we can attach a DisplayMessage component to any object, and it will appear above the character whenever we walk over it, if it has a trigger.

---
+++ Assets/Scripts/DisplayMessage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayMessage : MonoBehaviour
+{
+    public string messageToShow;
+
+    public string getMessage()
+    {
+        return messageToShow;
+    }
+
+}
