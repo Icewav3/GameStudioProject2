6bdbf1289c20a3bd6378f019f9ef3307425f534c
Cole Dorman <coledorman12@gmail.com>
2024-10-21 16:22:42 -0500

    ditto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -64,7 +64,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void OnDisable() //are we disabling player controller?
+    private void OnDisable()
     {
         move.Disable();
 
